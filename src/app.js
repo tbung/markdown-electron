94670ab210ef3efd33447e35fe71c62f55dd1887
Till Bungert <tillbungert@gmail.com>
2017-10-20 13:31:37 +0200

    Clean up code and project structure

---
+++ src/app.js
@@ -0,0 +1,60 @@
+import {ipcRenderer} from 'electron';
+import path from 'path';
+
+import * as mume from '@shd101wyy/mume';
+
+// Webview for markdown preview.
+// Please note that the webview will load
+// the controller script at:
+// https://github.com/shd101wyy/mume/blob/master/src/webview.ts
+const webview = document.createElement('webview');
+webview.style.width = '100%';
+webview.style.height = '100%';
+webview.style.border = 'none';
+webview.src = './loading.html';
+webview.preload = mume.utility.addFileProtocol(path.resolve(
+  mume.utility.extensionDirectoryPath,
+  './dependencies/electron-webview/preload.js'));
+
+document.body.appendChild(webview);
+
+webview.addEventListener('dom-ready', () => {
+  _webviewDOMReady = true;
+});
+
+/** 
+ * This method renders a markdown file in the webview created above
+ * @param {string} path - The path of the file to load
+ */
+async function renderMd(path) {
+  await mume.init();
+  const info = await mume.utility.tempOpen({prefix: 'mpe_preview',
+    suffix: '.html'});
+  let htmlFilePath = info.path;
+
+  const engine = new mume.MarkdownEngine({
+    filePath: path,
+    config: {
+      previewTheme: 'github-light.css',
+      codeBlockTheme: 'default.css',
+      printBackground: true,
+      mathRenderingOption: 'MathJax',
+    },
+  });
+
+  const html = await engine.generateHTMLTemplateForPreview({
+    head: '',
+    body: ' ',
+  });
+  await mume.utility.writeFile(htmlFilePath, html, {encoding: 'utf-8'});
+
+  if (webview.getURL() === htmlFilePath) {
+    webview.reload();
+  } else {
+    webview.loadURL(mume.utility.addFileProtocol(htmlFilePath));
+  }
+}
+
+ipcRenderer.on('file-opened', (event, file, content) => {
+  renderMd(file);
+});
---
+++ src/menu/menu-template.js
@@ -0,0 +1,132 @@
+import {dialog} from 'electron';
+
+/**
+ * Show openFile dialog and send the opend file to the renderer process
+ * @param {BrowserWindow} win - the currently active window
+ */
+function openFile(win) {
+  const files = dialog.showOpenDialog({
+    properties: ['openFile'],
+  });
+
+  if (!files) return;
+
+  const file = files[0];
+
+  win.webContents.send('file-opened', file);
+  win.setTitle(file);
+}
+
+module.exports = (win) => {
+  return [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Open...',
+          accelerator: 'CmdOrCtrl+O',
+          click: () => {
+            openFile(win);
+          },
+        },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall',
+        },
+      ],
+    },
+    {
+      label: 'Developer',
+      submenu: [
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: process.platform === 'darwin'
+          ? 'Alt+Command+I'
+          : 'Ctrl+Shift+I',
+          click: () => {
+            win.webContents.toggleDevTools();
+          },
+        },
+      ],
+    },
+  ];
+};
+// if (process.platform === 'darwin') {
+//     const name = app.getName()
+//     template.unshift({
+//         label: name,
+//         submenu: [
+//             {
+//                 label: 'About ' + name,
+//                 role: 'about'
+//             },
+//             {
+//                 type: 'separator'
+//             },
+//             {
+//                 label: 'Services',
+//                 role: 'services',
+//                 submenu: []
+//             },
+//             {
+//                 type: 'separator'
+//             },
+//             {
+//                 label: 'Hide ' + name,
+//                 accelerator: 'Command+H',
+//                 role: 'hide'
+//             },
+//             {
+//                 label: 'Hide Others',
+//                 accelerator: 'Command+Alt+H',
+//                 role: 'hideothers'
+//             },
+//             {
+//                 label: 'Show All',
+//                 role: 'unhide'
+//             },
+//             {
+//                 type: 'separator'
+//             },
+//             {
+//                 label: 'Quit',
+//                 accelerator: 'Command+Q',
+//                 click () { app.quit() }
+//             }
+//         ]
+//     })
+// }
